71321cafa554cb9b23e7e503db8fd035833b1ff3
Shreyas Chaudhari <47518683+schoudhari23@users.noreply.github.com>
2019-02-19 19:44:07 +0530

    Update assign2.js

--- assign1_2.js
+++ assign1_2.js
@@ -3,18 +3,16 @@ function calculateFrequency(string) {
     let set1 = new Set();
     let len = string.length;
     let freq = new Object();
-    //let objsz = 0;
-    for(let i=0; i < len; i++) {
-       if((string[i] >= "a" && string[i] <= "z" || string[i] >= "A" && string[i] <= "Z") && string[i]!=" " && !s1.has(string[i])) {
-          set1.add(string[i]);
-          let count = 1; 
-          for(let j=i+1; j < len; j++) {
-             if(string[j] == string[i])      
-                cnt++;
-          }
-          freq[string[i]] = count;   
-         // document.write(string[i]+": "+cnt+", ")
-       }
+    for (let i = 0; i < len; i++) {
+        if ((string[i] >= "a" && string[i] <= "z" || string[i] >= "A" && string[i] <= "Z") && string[i] != " " && !s1.has(string[i])) {
+            set1.add(string[i]);
+            let count = 1;
+            for (let j = i + 1; j < len; j++) {
+                if (string[j] == string[i])
+                    cnt++;
+            }
+            freq[string[i]] = count;
+        }
     }
     return freq;
 }
