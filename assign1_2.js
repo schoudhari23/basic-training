b6aac102a807fc36ed28116fe3568b42a86a968a
schoudhari <schoudhari@techracers.io>
2019-02-19 19:13:40 +0530

    Replaced 'var' by 'let' for all files

--- assign1_2.js
+++ assign1_2.js
@@ -1,14 +1,14 @@
 // Problem 2: Complete the calculateFrequency function that takes lowercase string as input and returns frequency of all english alphabet. (using only array, no in-built function)
 function calculateFrequency(string) {
-    var set1 = new Set();
-    var len = string.length;
-    var freq = new Object();
-    //var objsz = 0;
-    for(var i=0; i < len; i++) {
+    let set1 = new Set();
+    let len = string.length;
+    let freq = new Object();
+    //let objsz = 0;
+    for(let i=0; i < len; i++) {
        if((string[i] >= "a" && string[i] <= "z" || string[i] >= "A" && string[i] <= "Z") && string[i]!=" " && !s1.has(string[i])) {
           set1.add(string[i]);
-          var count = 1; 
-          for(var j=i+1; j < len; j++) {
+          let count = 1; 
+          for(let j=i+1; j < len; j++) {
              if(string[j] == string[i])      
                 cnt++;
           }
--- assign1_3.js
+++ assign1_3.js
@@ -1,10 +1,10 @@
 // Problem 3 : Complete the flatten function that takes a JS Object, returns a JS Object in flatten format (compressed)
 function flatten(obj) {
-    var newObj = {};
-    for(var key in obj) {
+    let newObj = {};
+    for(let key in obj) {
         if(obj[key].value !== null && typeof obj[key] === 'object') {
-            var temp = flatten(obj[key]);
-            for (var key2 in temp) {
+            let temp = flatten(obj[key]);
+            for (let key2 in temp) {
                 newObj[key+"."+key2] = temp[key2];
             }
         }
