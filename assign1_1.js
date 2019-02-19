91c9ce4e270aeaec35ba7bfedd8cc15ec91dcac9
schoudhari <schoudhari@techracers.io>
2019-02-19 19:13:40 +0530

    Replaced 'var' by 'let' for all files

--- assign1_1.js
+++ assign1_1.js
@@ -1,9 +1,9 @@
 // Problem 1: Complete the secondLargest function which takes in an array of numbers in input and return the second biggest number in the array. (without using sort)?
 function secondLargest(array) {
-  for(var i = 0; i < array.length; i++) {
-      for(var j=0; j < array.length-1; j++) {
+  for(let i = 0; i < array.length; i++) {
+      for(let j=0; j < array.length-1; j++) {
           if(array[j] > array[j+1]) {
-            var temp = array[j];
+            let temp = array[j];
             array[j] = array[j+1];
             array[j+1] = temp;
           }
