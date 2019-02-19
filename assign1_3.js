b507a0a8c1a50147e6cdc62a6e6a84927d83d637
Shreyas Chaudhari <47518683+schoudhari23@users.noreply.github.com>
2019-02-19 19:45:03 +0530

    Updated assign3.js

--- assign1_3.js
+++ assign1_3.js
@@ -1,14 +1,13 @@
 // Problem 3 : Complete the flatten function that takes a JS Object, returns a JS Object in flatten format (compressed)
 function flatten(obj) {
     let newObj = {};
-    for(let key in obj) {
-        if(obj[key].value !== null && typeof obj[key] === 'object') {
+    for (let key in obj) {
+        if (obj[key].value !== null && typeof obj[key] === 'object') {
             let temp = flatten(obj[key]);
             for (let key2 in temp) {
-                newObj[key+"."+key2] = temp[key2];
+                newObj[key + "." + key2] = temp[key2];
             }
-        }
-        else {
+        } else {
             newObj[key] = obj[key];
         }
     }
