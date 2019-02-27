3d8d6487e88d1a1f3d125aaa15263ca5c3a0277f
schoudhari <schoudhari@techracers.io>
2019-02-27 16:53:37 +0530

    Indentation done for index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+var fs = require("fs");
+var files = ["file1.txt", "file2.txt", "file3.txt", "file4.txt", "file5.txt"]
+
+var index = 0;
+fileRead(0);
+
+function fileRead(index) {
+    if (index == 5) {
+        console.log("Done")
+    } else {
+        fs.readFile(files[index], function(error, data) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(data.toString());
+                console.log(data.toString());
+                console.log("================");
+                fileRead(index + 1);
+            }
+        })
+    }
+}
