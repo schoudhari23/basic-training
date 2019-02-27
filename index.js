792c894a91a3b1888ee494b146949289c69e4ac2
schoudhari <schoudhari@techracers.io>
2019-02-27 15:42:53 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 $(function()
 {
     $("#regst").validate(
@@ -39,4 +40,27 @@ $(function()
         }
       });	
 });
+=======
+var fs = require("fs");
+var files = ["file1.txt","file2.txt","file3.txt","file4.txt","file5.txt"]
+
+var index = 0;
+fileRead(0);
+function fileRead(index) {
+	if(index == 5) {
+		console.log("Done")
+	} else {
+		fs.readFile(files[index], function(error,data) {
+			if(error) {
+				console.log(error);
+			} else {
+					console.log(data.toString());
+					console.log(data.toString());
+					console.log("================");
+					fileRead(index+1);
+				} 
+		})
+	}
+}
+>>>>>>> ee3642e... Reading content of file using callback
 
