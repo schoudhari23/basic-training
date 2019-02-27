4521a11524ce6c331581fd86f95afd0f3fd642bd
schoudhari <schoudhari@techracers.io>
2019-02-27 17:54:23 +0530

    Indentation done for reg.html and server.js

--- server.js
+++ server.js
@@ -20,4 +20,4 @@ http.createServer( function (request, response) {
    });   
 }).listen(8081);
 
-console.log('Server running at http://127.0.0.1:8081/');
+console.log("Server running at http://127.0.0.1:8081/");
