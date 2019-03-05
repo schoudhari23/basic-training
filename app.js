b8e8c1f1795830af242837119a4698a99ee1218f
schoudhari23 <schoudhari@techracers.io>
2019-03-05 16:59:55 +0530

    Added Tax Calculator

--- app.js
+++ app.js
@@ -1,84 +1,42 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var logger = require('morgan');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
-
-var index = require('./routes/index');
-var users = require('./routes/users');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.use(favicon());
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-/// catch 404 and forwarding to error handler
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  next(createError(404));
 });
 
-/// error handlers
+// error handler
+/*app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-// Handlebars default config
-const hbs = require('hbs');
-const fs = require('fs');
-
-const partialsDir = __dirname + '/views/partials';
-
-const filenames = fs.readdirSync(partialsDir);
-
-filenames.forEach(function (filename) {
-  const matches = /^([^.]+).hbs$/.exec(filename);
-  if (!matches) {
-    return;
-  }
-  const name = matches[1];
-  const template = fs.readFileSync(partialsDir + '/' + filename, 'utf8');
-  hbs.registerPartial(name, template);
-});
-
-hbs.registerHelper('json', function(context) {
-    return JSON.stringify(context, null, 2);
-});
-
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});*/
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,100 +1,139 @@
 var express = require('express');
 var router = express.Router();
+require('console.html');
+//console.html('<body><h1>Hello, world!</h1></body>');
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.render('index');
 });
 
-var food = ["chocolates","chocolate"]
+var food = ["chocolates", "chocolate"]
 var medicine = ["headache"];
 
-router.post('/', function(req, res) {
+router.post('/', function (req, res) {
   var problem = req.body.problem;
+  //res.write("Hello");
   //console.log(typeof problem);
-  if(IsJsonString(problem))
-  {
-    console.log("JSON");
+  if (IsJsonString(problem)) {
+    //console.log("JSON");
     var obj = JSON.parse(problem);
     //console.log(obj);
-    printR(obj);
+    printR(obj,res);
   } else {
-    console.log("plain");
+    //console.log("plain");
     var strings = problem.split("\n");
     console.log(strings);
     var output = {};
     strings[0] = strings[0].replace(/(\r\n|\n|\r)/gm, "");
     output["name"] = strings[0];
     var arrayObj = new Array();
-    for(var i=1;i<strings.length;i++)
-    {
+    for (var i = 1; i < strings.length; i++) {
       //console.log(strings[i]);
       var wor = strings[i].split(" ");
       //console.log(wor);
       var taxObj = {};
       taxObj["quantity"] = wor[0];
-      if(wor.includes("imported")) {
+      if (wor.includes("imported")) {
         taxObj["imported"] = 1;
         var index = wor.indexOf("imported");
         wor.splice(index, 1);
         //console.log(wor);
-        var objName = wor.slice(1,wor.length-2);
+        var objName = wor.slice(1, wor.length - 2);
         objName = objName.join(" ");
         //console.log(objName); 
         taxObj["name"] = objName;
       } else {
-        var objName = wor.slice(1,wor.length-2);
+        var objName = wor.slice(1, wor.length - 2);
         objName = objName.join(" ");
         //console.log(objName);
         taxObj["name"] = objName;
       }
-      if(wor.includes("chocolate") || wor.includes("chocolates")) {
+      if (wor.includes("chocolate") || wor.includes("chocolates")) {
         taxObj["category"] = "food";
-      } else if(wor.includes("book")) {
+      } else if (wor.includes("book") || wor.includes("books"))  {
         taxObj["category"] = "book";
-      } else if(wor.includes("pills")) {
+      } else if (wor.includes("pills") || wor.includes("pill") || wor.includes("headache")) {
         taxObj["category"] = "medicine";
       } else {
         taxObj["category"] = "payTax";
       }
 
-      var pri = wor[wor.length-1].replace(/(\r\n|\n|\r)/gm, "");
+      var pri = wor[wor.length - 1].replace(/(\r\n|\n|\r)/gm, "");
+     // console.log("SpePRICE:" + pri);
       taxObj['price'] = parseFloat(pri);
       //console.log(taxObj);
       arrayObj.push(taxObj);
       //output = jsonConcat(output, json1);
       //console.log(output);
     }
-   // console.log("Arrayyyy:"+arrayObj);
+    // console.log("Arrayyyy:"+arrayObj);
     output["items"] = arrayObj;
     //console.log(output);
-    printR(output);
+    printR(output,res);
   }
 
-  res.render('result');
+  //res.send('poasd');
 });
 
 module.exports = router;
 
 function IsJsonString(str) {
   try {
-      JSON.parse(str);
+    JSON.parse(str);
   } catch (e) {
-      return false;
+    return false;
   }
   return true;
 }
 
-function printR(jsObj)
-{
-  console.log(jsObj);
-  console.log(jsObj["name"]+":");
-  for(var i=0;i<jsObj.items.length;i++)
-  {
-    if(jsObj.items[i].hasOwnProperty("imported")) {
-      console.log(jsObj.items[i].quantity+" imported "+jsObj.items[i].name+": "+jsObj.items[i].price);
+function printR(jsObj,res) {
+  //res.write(jsObj+":");
+  var factor = 0.05;
+  res.write(jsObj["name"]);
+  var totalSalesTax = 0;
+  var totalPrice = 0;
+  for (var i = 0; i < jsObj.items.length; i++) {
+
+    if (jsObj.items[i].hasOwnProperty("imported")) {
+      if (jsObj.items[i].category == 'food' || jsObj.items[i].category == 'medicine' || jsObj.items[i].category == 'book') {
+        var x = Math.round((jsObj.items[i].price * jsObj.items[i].quantity * 0.05) / factor) * factor;
+        totalSalesTax += x;
+        var y = jsObj.items[i].price * jsObj.items[i].quantity + x;
+        jsObj.items[i].price = Math.round(y * 100) / 100;
+      } else {
+        var x = Math.round((jsObj.items[i].price * jsObj.items[i].quantity * 0.15) / factor) * factor;
+        totalSalesTax += x;
+        var y = jsObj.items[i].quantity * jsObj.items[i].price + x;
+        jsObj.items[i].price = Math.round(y * 100) / 100;
+      }
+      res.write("\n"+jsObj.items[i].quantity + " imported " + jsObj.items[i].name + ": " + jsObj.items[i].price);
+      totalPrice += jsObj.items[i].price;
     } else {
-      console.log(jsObj.items[i].quantity+" "+jsObj.items[i].name+": "+jsObj.items[i].price);
+      if (jsObj.items[i].category == 'food' || jsObj.items[i].category == 'medicine' || jsObj.items[i].category == 'book') {
+        var y = jsObj.items[i].quantity * jsObj.items[i].price ;
+        jsObj.items[i].price = Math.round(y * 100) / 100;
+      } else {
+        var x = Math.round((jsObj.items[i].price * jsObj.items[i].quantity * 0.10) / factor) * factor;
+        totalSalesTax += x;
+        var y = jsObj.items[i].price * jsObj.items[i].quantity + x;
+        jsObj.items[i].price = Math.round(y * 100) / 100;
+      }
+      res.write("\n"+jsObj.items[i].quantity + " " + jsObj.items[i].name + ": " + jsObj.items[i].price);
+      totalPrice += jsObj.items[i].price;
     }
   }
+  res.write("\nSales Taxes: " + Math.round(totalSalesTax * 100) / 100);
+  res.write("\nTotal: " + totalPrice);
+  res.end();
+}
+
+function calcSales(itemPrice,quant,taxRate)
+{
+  var factor = 0.5
+  var x = Math.round((itemPrice * quant * taxRate) / factor) * factor;
+  totalSalesTax += x;
+  var y =Itemprice * quant + x;
+  var price = Math.round(y * 100) / 100;
+  return price;
 }
