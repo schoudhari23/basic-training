4be7210f5b1a42ee6026f19eadbd4dd390317032
schoudhari23 <schoudhari@techracers.io>
2019-03-05 19:36:30 +0530

    Added README.md

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-Basic-training
+# Receipt Generator
+
+Receipt generator is a simple application built using node.js and express to calculate receipt details. 
+
+  - Type your input in string or JSON object format
+  - Click submit
+  - Get the receipt
+
+### Tech
+
+Receipt generator uses a number of open source projects to work properly:
+
+* [node.js] - evented I/O for the backend
+* [Express] - fast node.js network app framework
+
+
+### Installation
+1. Create a folder to hold your installation: 
+```sh
+mkdir tax
+```
+2. Copy the contents of the zip to your newly created folder
+3. Install the dependencies  and start the server.
+
+```sh
+$ cd tax
+```
+4. Install dependencies:
+```sh
+npm install
+```
+5. install nodemon as a development dependency:
+```sh
+npm install --save-dev nodemon
+```
+6. Start application
+```sh
+$ nodemon
+```
+
+
+
+[//]: # (These are reference links used in the body of this note and get stripped out when the markdown processor does its job. There is no need to format nicely because it shouldn't be seen. Thanks SO - http://stackoverflow.com/questions/4823468/store-comments-in-markdown-syntax)
+
+
+   
+   [node.js]: <http://nodejs.org>
+   [express]: <http://expressjs.com>
+   
+
--- routes/index.js
+++ routes/index.js
@@ -2,118 +2,108 @@ var express = require('express');
 var router = express.Router();
 require('console.html');
 
-/* GET home page. */
-router.get('/', function (req, res) {
-  res.render('index');
+router.get('/', function(req, res) {
+    res.render('index');
 });
 
 var food = ["chocolates", "chocolate"]
 var medicine = ["headache"];
 
-router.post('/', function (req, res) {
-  var problem = req.body.problem;
-  if (IsJsonString(problem)) {
-    var obj = JSON.parse(problem);
-    printR(obj, res);
-  } else {
-    var strings = problem.split("\n");
-    console.log(strings);
-    var output = {};
-    strings[0] = strings[0].replace(/(\r\n|\n|\r)/gm, "");
-    output["name"] = strings[0];
-    var arrayObj = new Array();
-    for (var i = 1; i < strings.length; i++) {
-      var wor = strings[i].split(" ");
-      var taxObj = {};
-      taxObj["quantity"] = wor[0];
-      if (wor.includes("imported")) {
-        taxObj["imported"] = 1;
-        var index = wor.indexOf("imported");
-        wor.splice(index, 1);
-        var objName = wor.slice(1, wor.length - 2);
-        objName = objName.join(" ");
-        taxObj["name"] = objName;
-      } else {
-        var objName = wor.slice(1, wor.length - 2);
-        objName = objName.join(" ");
-        taxObj["name"] = objName;
-      }
-      if (wor.includes("chocolate") || wor.includes("chocolates")) {
-        taxObj["category"] = "food";
-      } else if (wor.includes("book") || wor.includes("books")) {
-        taxObj["category"] = "book";
-      } else if (wor.includes("pills") || wor.includes("pill") || wor.includes("headache")) {
-        taxObj["category"] = "medicine";
-      } else {
-        taxObj["category"] = "payTax";
-      }
+router.post('/', function(req, res) {
+    var problem = req.body.problem;
+    if (IsJsonString(problem)) {
+        var obj = JSON.parse(problem);
+        printR(obj, res);
+    } else {
+        var strings = problem.split("\n");
+        console.log(strings);
+        var output = {};
+        strings[0] = strings[0].replace(/(\r\n|\n|\r)/gm, "");
+        output["name"] = strings[0];
+        var arrayObj = new Array();
+        for (var i = 1; i < strings.length; i++) {
+            var wor = strings[i].split(" ");
+            var taxObj = {};
+            taxObj["quantity"] = wor[0];
+            if (wor.includes("imported")) {
+                taxObj["imported"] = 1;
+                var index = wor.indexOf("imported");
+                wor.splice(index, 1);
+                var objName = wor.slice(1, wor.length - 2);
+                objName = objName.join(" ");
+                taxObj["name"] = objName;
+            } else {
+                var objName = wor.slice(1, wor.length - 2);
+                objName = objName.join(" ");
+                taxObj["name"] = objName;
+            }
+            if (wor.includes("chocolate") || wor.includes("chocolates")) {
+                taxObj["category"] = "food";
+            } else if (wor.includes("book") || wor.includes("books")) {
+                taxObj["category"] = "book";
+            } else if (wor.includes("pills") || wor.includes("pill") || wor.includes("headache")) {
+                taxObj["category"] = "medicine";
+            } else {
+                taxObj["category"] = "payTax";
+            }
 
-      var pri = wor[wor.length - 1].replace(/(\r\n|\n|\r)/gm, "");
-      taxObj['price'] = parseFloat(pri);
-      arrayObj.push(taxObj);
+            var pri = wor[wor.length - 1].replace(/(\r\n|\n|\r)/gm, "");
+            taxObj["price"] = parseFloat(pri);
+            arrayObj.push(taxObj);
+        }
+
+        output["items"] = arrayObj;
+        printR(output, res);
     }
-
-    output["items"] = arrayObj;
-    printR(output, res);
-  }
 });
 
 module.exports = router;
 
 function IsJsonString(str) {
-  try {
-    JSON.parse(str);
-  } catch (e) {
-    return false;
-  }
-  return true;
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
 }
 
 function printR(jsObj, res) {
-  var factor = 0.05;
-  res.write(jsObj["name"]);
-  var totalSalesTax = 0;
-  var totalPrice = 0;
-  for (var i = 0; i < jsObj.items.length; i++) {
+    var factor = 0.05;
+    res.write(jsObj["name"]);
+    var totalSalesTax = 0;
+    var totalPrice = 0;
+    for (var i = 0; i < jsObj.items.length; i++) {
 
-    if (jsObj.items[i].hasOwnProperty("imported")) {
-      if (jsObj.items[i].category == 'food' || jsObj.items[i].category == 'medicine' || jsObj.items[i].category == 'book') {
-        var x = Math.round((jsObj.items[i].price * jsObj.items[i].quantity * 0.05) / factor) * factor;
-        totalSalesTax += x;
-        var y = jsObj.items[i].price * jsObj.items[i].quantity + x;
-        jsObj.items[i].price = Math.round(y * 100) / 100;
-      } else {
-        var x = Math.round((jsObj.items[i].price * jsObj.items[i].quantity * 0.15) / factor) * factor;
-        totalSalesTax += x;
-        var y = jsObj.items[i].quantity * jsObj.items[i].price + x;
-        jsObj.items[i].price = Math.round(y * 100) / 100;
-      }
-      res.write("\n" + jsObj.items[i].quantity + " imported " + jsObj.items[i].name + ": " + jsObj.items[i].price);
-      totalPrice += jsObj.items[i].price;
-    } else {
-      if (jsObj.items[i].category == 'food' || jsObj.items[i].category == 'medicine' || jsObj.items[i].category == 'book') {
-        var y = jsObj.items[i].quantity * jsObj.items[i].price;
-        jsObj.items[i].price = Math.round(y * 100) / 100;
-      } else {
-        var x = Math.round((jsObj.items[i].price * jsObj.items[i].quantity * 0.10) / factor) * factor;
-        totalSalesTax += x;
-        var y = jsObj.items[i].price * jsObj.items[i].quantity + x;
-        jsObj.items[i].price = Math.round(y * 100) / 100;
-      }
-      res.write("\n" + jsObj.items[i].quantity + " " + jsObj.items[i].name + ": " + jsObj.items[i].price);
-      totalPrice += jsObj.items[i].price;
+        if (jsObj.items[i].hasOwnProperty("imported")) {
+            if (jsObj.items[i].category == 'food' || jsObj.items[i].category == 'medicine' || jsObj.items[i].category == 'book') {
+                var x = Math.round((jsObj.items[i].price * jsObj.items[i].quantity * 0.05) / factor) * factor;
+                totalSalesTax += x;
+                var y = jsObj.items[i].price * jsObj.items[i].quantity + x;
+                jsObj.items[i].price = Math.round(y * 100) / 100;
+            } else {
+                var x = Math.round((jsObj.items[i].price * jsObj.items[i].quantity * 0.15) / factor) * factor;
+                totalSalesTax += x;
+                var y = jsObj.items[i].quantity * jsObj.items[i].price + x;
+                jsObj.items[i].price = Math.round(y * 100) / 100;
+            }
+            res.write("\n" + jsObj.items[i].quantity + " imported " + jsObj.items[i].name + ": " + jsObj.items[i].price);
+            totalPrice += jsObj.items[i].price;
+        } else {
+            if (jsObj.items[i].category == 'food' || jsObj.items[i].category == 'medicine' || jsObj.items[i].category == 'book') {
+                var y = jsObj.items[i].quantity * jsObj.items[i].price;
+                jsObj.items[i].price = Math.round(y * 100) / 100;
+            } else {
+                var x = Math.round((jsObj.items[i].price * jsObj.items[i].quantity * 0.10) / factor) * factor;
+                totalSalesTax += x;
+                var y = jsObj.items[i].price * jsObj.items[i].quantity + x;
+                jsObj.items[i].price = Math.round(y * 100) / 100;
+            }
+            res.write("\n" + jsObj.items[i].quantity + " " + jsObj.items[i].name + ": " + jsObj.items[i].price);
+            totalPrice += jsObj.items[i].price;
+        }
     }
-  }
-  res.write("\nSales Taxes: " + Math.round(totalSalesTax * 100) / 100);
-  res.write("\nTotal: " + totalPrice);
-  res.end();
-}
-
-function calcSales(itemPrice, quant, taxRate) {
-  var factor = 0.5
-  var x = Math.round((itemPrice * quant * taxRate) / factor) * factor;
-  totalSalesTax += x;
-  var y = Itemprice * quant + x;
-  var price = Math.round(y * 100) / 100;
-  return price;
+    res.write("\nSales Taxes: " + Math.round(totalSalesTax * 100) / 100);
+    res.write("\nTotal: " + totalPrice);
+    res.end();
 }
