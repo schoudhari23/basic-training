358342f31b4d156ec7033ab90f518259d07d7820
schoudhari23 <schoudhari@techracers.io>
2019-03-08 11:12:01 +0530

    Changes in index.js

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,14 @@ var router = express.Router();
 require('console.html');
 
 var method = require('../controller/index.js');
-
-router.get('/', function(req, res) {
+console.log(method);
+router.get('/', function (req, res) {
     res.render('index');
 });
 
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
     res.write(method(req.body.problem));
     res.end();
 });
 
 module.exports = router;
-
